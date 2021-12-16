11154eb1487de4dc66199632bf8b2754e8b553f2
Patrick Brennan <pbrennanmusic@gmail.com>
2021-12-16 00:04:37 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -11,3 +11,12 @@ let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
 changeState(state, action)
+
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+function render() {
+  document.body.textContent = state.count
+}
